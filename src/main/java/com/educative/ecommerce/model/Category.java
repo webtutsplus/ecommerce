6018f62f8b1a3084ad1be30e0587ed4f8d0d6238
nmadhab <nilmadhab.mondal@booking.com>
2021-08-24 22:50:27 +0200

    category apis created

---
+++ src/main/java/com/educative/ecommerce/model/Category.java
@@ -0,0 +1,81 @@
+package com.educative.ecommerce.model;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "category_name")
+	private @NotBlank String categoryName;
+
+	private @NotBlank String description;
+
+	private @NotBlank String imageUrl;
+
+
+	public Category() {
+	}
+
+	public Category(@NotBlank String categoryName, @NotBlank String description) {
+		this.categoryName = categoryName;
+		this.description = description;
+	}
+
+	public Category(@NotBlank String categoryName, @NotBlank String description, @NotBlank String imageUrl) {
+		this.categoryName = categoryName;
+		this.description = description;
+		this.imageUrl = imageUrl;
+	}
+
+	public String getCategoryName() {
+		return this.categoryName;
+	}
+
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "User {category id=" + id + ", category name='" + categoryName + "', description='" + description + "'}";
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}
