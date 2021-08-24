1ddb2ff1a84253d8fcccb832a083a81de190232c
nmadhab <nilmadhab.mondal@booking.com>
2021-08-24 21:47:06 +0200

    first commit

---
+++ src/test/java/com/educative/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educative.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
