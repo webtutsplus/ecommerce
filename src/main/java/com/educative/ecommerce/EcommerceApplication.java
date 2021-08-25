24d63def783ece381a332a689fcee594c4fb82b5
nmadhab <nilmadhab.mondal@booking.com>
2021-08-25 20:46:26 +0200

    initial project setup

--- src/main/java/com/educative/ecommerce/EcommerceApplication.java
+++ src/main/java/com/educative/ecommerce/EcommerceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
-
 }
