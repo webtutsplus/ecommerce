0e944c2340d845d1be2652a7e13eec18c701a3ed
nmadhab <nilmadhab.mondal@booking.com>
2021-09-09 08:15:46 +0200

    custom exceptions added

---
+++ src/main/java/com/educative/ecommerce/exceptions/ExceptionControllerAdvice.java
@@ -0,0 +1,15 @@
+package com.educative.ecommerce.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+    @ExceptionHandler(value = CustomException.class)
+    public final ResponseEntity<String> handleUpdateFailException(CustomException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
